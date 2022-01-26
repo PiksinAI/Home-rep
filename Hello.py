b09f387c1177df1d24839691dafacf538f15d6d7
Alexey Piksin <hekpojiog@list.ru>
2022-01-26 21:38:18 +0300

    test changes

--- Hello.py
+++ Hello.py
@@ -1,5 +1,15 @@
-print('Hello world')
-s = 12323435;
-d = 'fgfjdhg';
-s = str(s)
-print(d + s)
+print('Задача 6. Маятник ')
+
+
+def fading(ampl):
+    ampl = ampl * 0.916
+    return ampl
+
+
+entire_ampl = float(input('Введите начальную амплитуду маятника: '))
+stop_ampl = float(input('Введите амплитуду остановки: '))
+count = 0
+while entire_ampl > stop_ampl:
+    entire_ampl = fading(entire_ampl)
+    count += 1
+print('Маятник качнется раз:', count)
