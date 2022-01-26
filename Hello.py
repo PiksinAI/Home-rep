2d7480c40af8d713e045a1a715f0505047cf24ca
Alexey Piksin <hekpojiog@list.ru>
2022-01-26 21:18:38 +0300

    pendolum programm added

--- Hello.py
+++ Hello.py
@@ -1,1 +1,17 @@
-print('Hello world')
+print('Задача 6. Маятник ')
+
+
+def fading(ampl):
+    ampl = ampl * 0.916
+    return ampl
+
+
+entire_ampl = float(input('Введите начальную амплитуду маятника: '))
+
+
+stop_ampl = float(input('Введите амплитуду остановки: '))
+count = 0
+while entire_ampl > stop_ampl:
+    entire_ampl = fading(entire_ampl)
+    count += 1
+print('Маятник качнется раз:',count)
